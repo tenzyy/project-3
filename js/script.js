03a85b4d1d26488cff2f877fc8817f5285a75084
Mac <mac2018@Macs-iMac-6.local>
2019-05-06 15:35:15 -0700

    using css grid,mobile,tablet and desktop media query changes

--- js/script.js
+++ js/script.js
@@ -13,20 +13,31 @@ $('#article-select').on('change', function () {
             url: " https://api.nytimes.com/svc/topstories/v2/" + selectedCategory + ".json?api-key=0rDLQOWB9Rgp7vmdbqIgHe7T5j6S389t"
         })
             .done(function (data) {
+                const dataFiltered = data.results.filter(function(article) {
+                    return article.multimedia[4] !==  undefined;
+                });
+               
+                const dataFilterSliced = dataFiltered.slice(0,12);
 
                 // filter data and filter data. results 
                 // get rid of other multimedia urls that don't exist 
-                
-                 $(".newsarticles").empty();
-                $.each(data.results, function (index, article) {
+
+                //  $(".newsarticles").empty();
+                $.each(dataFilterSliced, function (index, article) {
                     const content = article.abstract;
                     const title = article.title;
                     const image = article.multimedia[4].url;
                     const url = article.url;
+                   
                     
-                    $(".newsarticles").append(`<a href ="${url}"><p>${content}</p><p>${title}</p><p><img src ="${image}"/></p></a>`);
+                    $(".newsarticles").append(`<a href ="${url}">
+                    <p class ="content">${content}</p></div>
+                    <title>${title}</title>
+                    <img class= "articleimage" src = "${image}"/></a>`);
 
 
+                
+                
                 });
             });
     }

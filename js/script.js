29d0dcef08f2daeedff7dd15e806ec1fa8ad833a
Andrew DiBiase <dibiase.andrew@outlook.com>
2019-05-16 22:09:01 -0700

    apply changes outlined in issues

--- js/script.js
+++ js/script.js
@@ -1,50 +1,44 @@
-
-
-$('#article-select').on('change', function () {
+$(function() {
+  $("#article-select").on("change", function() {
     const selectedCategory = $(this).val();
     if (selectedCategory == "nooption") {
-        $(".newsarticles").html("");
-    }
-    else if (selectedCategory !== '') {
-        $(".loader").show();
+      $(".newsarticles").html("");
+    } else if (selectedCategory !== "") {
+      $(".loader").show();
 
+      $.ajax({
+        method: "get",
+        url:
+          " https://api.nytimes.com/svc/topstories/v2/" +
+          selectedCategory +
+          ".json?api-key=0rDLQOWB9Rgp7vmdbqIgHe7T5j6S389t"
+      })
 
-        $.ajax({
-            method: 'get',
-            url: " https://api.nytimes.com/svc/topstories/v2/" + selectedCategory + ".json?api-key=0rDLQOWB9Rgp7vmdbqIgHe7T5j6S389t"
-        })
+        .done(function(data) {
+          $(".loader").hide(data);
+          $(".newsarticles").html("");
+          const dataFiltered = data.results
+            .filter(function(article) {
+              return article.multimedia[4] !== undefined;
+            })
+            .slice(0, 12);
+          $.each(dataFiltered, function(index, article) {
+            const content = article.abstract;
+            const title = article.title;
+            const image = article.multimedia[4].url;
+            const url = article.url;
 
-
-            .done(function (data) {
-                $(".loader").hide(data);
-                $(".newsarticles").html("");
-                const dataFiltered = data.results.filter(function (article) {
-                    return article.multimedia[4] !== undefined;
-                }).slice(0, 12);
-                $.each(dataFiltered, function (index, article) {
-                    const content = article.abstract;
-                    const title = article.title;
-                    const image = article.multimedia[4].url;
-                    const url = article.url;
-
-
-                    $(".newsarticles").append(`<a href ="${url}">
-                    <p class ="content">${content}</p></div>
+            $(
+              ".newsarticles"
+            ).append(`<a href ="${url}"><img class= "articleimage" src = "${image}"/>
+                    <p class ="content">${content}</p>
                     <title>${title}</title>
-                    <img class= "articleimage" src = "${image}"/></a>`);
-                });
-
-            }).fail(function () {
-                $('.newsarticles').append('Sorry there was an error.');
-
-
-
-            });
+                    </a>`);
+          });
+        })
+        .fail(function() {
+          $(".newsarticles").append("Sorry there was an error.");
+        });
     }
-
-
-
+  });
 });
-
-
-
